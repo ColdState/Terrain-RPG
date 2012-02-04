984eeaa38990e12812829f19a44062be44f5ee6b
Johnicholas Hines <johnicholas.hines@gmail.com>
2012-02-04 09:49:47 -0800

    Add some sort of terrain smoothing, distinguish herbivores and carnivores (by color, at least).

--- terrainrpg.js
+++ terrainrpg.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var map_width = 100; // blocks
+var map_height = 100; // blocks
+var cell_size = 32; // pixels
+
 function random(max) {
     return Math.floor(Math.random() * (max + 1));
 };
@@ -15,23 +19,6 @@ function wrap(value, max) {
     return value;
 }
 
-function Critter(options) {
-    this.sprite = options.sprite;
-    this.x = options.x;
-    this.y = options.y;
-    this.my_rect = new jaws.Rect(this.x, this.y, 10, 10);
-};
-Critter.prototype.draw = function () {
-    this.my_rect.draw();
-};
-Critter.prototype.rect = function () {
-    return this.my_rect;
-};
-Critter.prototype.move = function(dx, dy) {
-    this.x += dx;
-    this.y += dy;
-};
-
 function maybeMove(player, x, y, map) {
     player.move(x, y);
     var possible_obstacles = map.atRect(player.rect());
@@ -45,10 +32,101 @@ function maybeMove(player, x, y, map) {
     }
 };
 
+function Herbivore(options) {
+    this.sprite = options.sprite;
+    this.x = options.x;
+    this.y = options.y;
+    this.my_rect = new jaws.Rect(options.x, options.y, 10, 10);
+};
+Herbivore.prototype.draw = function () {
+    jaws.context.fillStyle = 'blue';
+    jaws.context.fillRect(this.my_rect.x, this.my_rect.y, 10, 10);
+};
+Herbivore.prototype.rect = function () {
+    return this.my_rect;
+};
+Herbivore.prototype.move = function (dx, dy) {
+    this.my_rect.move(dx, dy);
+}
+Herbivore.prototype.steer = function (map) {
+    switch(random(3)) {
+    case 0: maybeMove(this, -2, 0, map); break;
+    case 1: maybeMove(this, 2, 0, map); break;
+    case 2: maybeMove(this, 0, -2, map); break;
+    case 3: maybeMove(this, 0, 2, map); break;
+    }
+};
+
+function Carnivore(options) {
+    this.sprite = options.sprite;
+    this.x = options.x;
+    this.y = options.y;
+    this.my_rect = new jaws.Rect(options.x, options.y, 10, 10);
+};
+Carnivore.prototype.draw = function () {
+    jaws.context.fillStyle = 'red';
+    jaws.context.fillRect(this.my_rect.x, this.my_rect.y, 10, 10);
+};
+Carnivore.prototype.rect = function () {
+    return this.my_rect;
+};
+Carnivore.prototype.move = function(dx, dy) {
+    this.my_rect.move(dx, dy);
+};
+Carnivore.prototype.steer = function (map) {
+    switch(random(3)) {
+    case 0: maybeMove(this, -2, 0, map); break;
+    case 1: maybeMove(this, 2, 0, map); break;
+    case 2: maybeMove(this, 0, -2, map); break;
+    case 3: maybeMove(this, 0, 2, map); break;
+    }
+};
+
+function fillMap(map) {
+    for (var row = 0; row < map_width; ++row) {
+	for (var col = 0; col < map_height; ++col) {
+	    var options = {
+		rect: new jaws.Rect(row * cell_size, col * cell_size,
+				    cell_size, cell_size)};
+	    var tile;
+	    if (row === 0 ||
+		row + 1 === map_width ||
+		col === 0 ||
+		col + 1 === map_height) {
+		// borders are always mountains
+		tile = new Mountain(options);
+	    } else {
+		switch (random(1)) {
+		case 0:
+		    // generate randomly
+		    // Mountain is disallowed except on edges for now
+		    switch (random(2)+1) {
+		    case 0: tile = new Mountain(options); break;
+		    case 1: tile = new Forest(options); break;
+		    case 2: tile = new Grass(options); break;
+		    case 3: tile = new Lake(options); break;
+		    }
+		    break;
+		case 1:
+		    // duplicate something nearby
+		    // there always is something nearby,
+		    // because of the mountains on the border.
+		    var neighbors = map.atRect({
+			x: options.rect.x - cell_size,
+			y: options.rect.y - cell_size,
+			right: options.rect.right + cell_size,
+			bottom: options.rect.bottom + cell_size});
+		    var neighbor = neighbors[random(neighbors.length - 1)];
+		    tile = neighbor.clone(options);
+		    break;
+		}
+	    }
+	    map.push(tile);
+	}
+    }
+}
+
 function TerrainRPG(jaws) {
-    var map_width = 700; // blocks
-    var map_height = 700; // blocks
-    var cell_size = 32; // pixels
 
     var game = {};    
     
@@ -59,30 +137,15 @@ function TerrainRPG(jaws) {
 					   max_y: map_height * cell_size});
 
         // A tilemap, each cell is 32x32 pixels.
+
+	// We generate a first map,
+	// then we generate a second one from the first one.
+        var first_map = new jaws.TileMap({size: [map_width, map_height],
+					  cell_size: [32, 32]});
+	fillMap(first_map);
         this.tile_map = new jaws.TileMap({size: [map_width, map_height],
 					  cell_size: [32, 32]});
-	for (var row = 0; row < map_width; ++row) {
-	    for (var col = 0; col < map_height; ++col) {
-		var options = { rect: new jaws.Rect(row * cell_size,
-						    col * cell_size,
-						    cell_size, cell_size)};
-		var tile;
-		if (row === 0 ||
-		    row + 1 === map_width ||
-		    col === 0 ||
-		    col + 1 === map_height) {
-		    tile = new Mountain(options);
-		} else {
-		    switch (random(3)) {
-		    case 0: tile = new Mountain(options); break;
-		    case 1: tile = new Forest(options); break;
-		    case 2: tile = new Grass(options); break;
-		    case 3: tile = new Lake(options); break;
-		    }
-		}
-		this.tile_map.push(tile);
-	    }
-	}
+	fillMap(this.tile_map);
 
         this.player = new jaws.Sprite({x:250, y:250,
 				       scale: 2, anchor: "center"});
@@ -98,9 +161,16 @@ function TerrainRPG(jaws) {
         this.player.setImage( this.player.anim_default.next() );
         jaws.preventDefaultKeys(["up", "down", "left", "right", "space"]);
         this.critters = [];
-	for (var i = 0; i < 10; ++i) {
-	    this.critters.push(new Critter({x: random(jaws.width),
-					    y: random(jaws.height)}));
+	for (var i = 0; i < 100; ++i) {
+	    var options = {x: random(map_width * cell_size),
+			   y: random(map_height * cell_size)};
+	    var critter;
+	    if (random(10) < 8) {
+		critter = new Herbivore(options);
+	    } else {
+		critter = new Carnivore(options);
+	    }
+	    this.critters.push(critter);
 	}
     }
 
@@ -127,12 +197,7 @@ function TerrainRPG(jaws) {
 
         this.viewport.centerAround(this.player)
 	for (var i in this.critters) {
-	    switch(random(3)) {
-	    case 0: this.critters[i].move(-10, 0); break;
-	    case 1: this.critters[i].move(10, 0); break;
-	    case 2: maybeMove(this.critters[i], 0, -1 * cell_size, this.tile_map); break;
-	    case 3: maybeMove(this.critters[i], 0, cell_size, this.tile_map); break;
-	    }
+	    this.critters[i].steer(this.tile_map);
 	}
     }
 
--- terraintypes.js
+++ terraintypes.js
@@ -25,6 +25,9 @@ Mountain.prototype.draw = function () {
 };
 // In general, mountains are impassable
 Mountain.prototype.impassable = true;
+Mountain.prototype.clone = function (options) {
+    return new Mountain(options);
+};
 
 function Forest(options) {
 	options.sprite = new jaws.Sprite({
@@ -33,22 +36,32 @@ function Forest(options) {
     TerrainTile.call(this, options);
 };
 Object.extend(Forest, TerrainTile);
+Forest.prototype.clone = function (options) {
+    return new Forest(options);
+};
 
 function Grass(options) {
-	options.sprite = new jaws.Sprite({
-		image: 'grass_x32.png',
-		x: 0, y: 0});
+    options.sprite = new jaws.Sprite({
+	image: 'grass_x32.png',
+	x: 0, y: 0});
     TerrainTile.call(this, options);
 };
 Object.extend(Grass, TerrainTile);
+Grass.prototype.clone = function (options) {
+    return new Grass(options);
+};
 
 function Lake(options) {
     TerrainTile.call(this, options);
 };
 Object.extend(Lake, TerrainTile);
 Lake.prototype.draw = function () {
-    jaws.context.fillStyle = 'blue';
+    jaws.context.fillStyle = 'cyan';
     jaws.context.fillRect(this.my_rect.x, this.my_rect.y,
 			  this.my_rect.width, this.my_rect.height);
+};
+Lake.prototype.clone = function (options) {
+    // When you clone a lake you actually get a forest
+    return new Forest(options);
 };
 
