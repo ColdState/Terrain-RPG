1f7207af177d31ae9437604df29f2a6f3f320194
Johnicholas Hines <johnicholas.hines@gmail.com>
2012-03-08 08:04:15 -0500

    Change to smoother motion

---
+++ config.js
@@ -0,0 +1,7 @@
+var map_width = 30; // blocks
+var map_height = 30; // blocks
+var cell_size = 32; // pixels
+var initial_critter_count = 20;
+var herbivore_probability = 50; // percent
+var ticks_per_turn = 45  // Controls how many update() ticks per turn
+
